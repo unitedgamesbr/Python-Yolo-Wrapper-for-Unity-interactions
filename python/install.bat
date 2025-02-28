d80b4d5a1f1ffe9cc3df95c5561acfe5f2d61869
Thales Silva <thalesv.profissional@gmail.com>
2025-02-28 19:23:41 -0300

    [FIX]  PyTorch CUDA Install
    - Forcing reinstall of the correct version of PyTorch

--- python/install.bat
+++ python/install.bat
@@ -3,10 +3,12 @@ echo Installation process...
 
 echo Installing dependencies from requirements.txt...
 pip install --force-reinstall -r requirements.txt
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
 echo Installing package...
 pip install -e .
+
+echo Installing PyTorch CUDA 11.8
+pip install --upgrade --force-reinstall  torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu118
 
 echo Done.
 pause
